deaf5271b9f256e0099eb2862444bd42280e77f7
Clair Sun <sijings@andrew.cmu.edu>
2019-12-04 17:13:58 -0500

    Bar_Graphs

--- Final Project/Graphs.js
+++ Final Project/Graphs.js
@@ -1,3 +1,8 @@
+var options={
+  tooltip:{theme:'custom'},
+    actions:false
+}
+
 var Ratings_TR = {
         $schema: 'https://vega.github.io/schema/vega-lite/v2.0.json',
         description: 'A simple bar chart with embedded data.',
@@ -46,5 +51,157 @@ var Ratings_TR = {
           },
         }
       };
-      vegaEmbed('#Ratings_TR', Ratings_TR);
+      vegaEmbed('#Ratings_TR', Ratings_TR,options);
 
+
+var Ratings_TD = {
+        $schema: 'https://vega.github.io/schema/vega-lite/v2.0.json',
+        description: 'A simple bar chart with embedded data.',
+        data: {url:"Real_D.csv"},
+        background:"transparent",
+        mark: 'bar',
+        width:300,
+        height:300,
+        "selection": {
+          "highlight": {"type": "single", "empty": "none", "on": "mouseover"},
+          "select": {"type": "multi"}
+        },
+        "mark": {
+          "type": "bar",
+          "fill": "#ffa500",
+          "cursor": "pointer"
+        },
+        title: {
+                  "text":"Review Rating & True Don't Recommand",
+                  "color": "#fff"
+              },
+        encoding: {
+          x: {
+            "bin": "false",
+            "field": "ratings",
+            "type": "ordinal",
+
+            "orient": "right",
+            "axis": {title: 'Review Rating', "titleColor":"#fff",labelColor:"#fff" }
+          },
+          y: {
+            "aggregate": "sum",
+            "field": "ratings",
+            "type": "quantitative",
+            "axis": {title: "Numbers of True Don't Recommand Reviews", "titleColor":"#fff", labelColor:"#fff" }
+          },
+          "tooltip": [{"aggregate": "sum","field": "ratings", "type": "quantitative"}],
+          "color": {"value": "#ffa500"},
+          "fillOpacity": {
+             "condition": [
+              {
+                "selection": "highlight", "value": 1
+              }
+          ],
+          "value": 0.5
+          },
+        }
+      };
+      vegaEmbed('#Ratings_TD', Ratings_TD, options);
+
+
+var Ratings_FD = {
+        $schema: 'https://vega.github.io/schema/vega-lite/v2.0.json',
+        description: 'A simple bar chart with embedded data.',
+        data: {url:"Fake_D.csv"},
+        background:"transparent",
+        mark: 'bar',
+        width:300,
+        height:300,
+        "selection": {
+          "highlight": {"type": "single", "empty": "none", "on": "mouseover"},
+          "select": {"type": "multi"}
+        },
+        "mark": {
+          "type": "bar",
+          "fill": "#ffa500",
+          "cursor": "pointer"
+        },
+        title: {
+                  "text":"Review Rating & Fake Don't Recommand",
+                  "color": "#fff"
+              },
+        encoding: {
+          x: {
+            "bin": "false",
+            "field": "ratings",
+            "type": "ordinal",
+
+            "orient": "right",
+            "axis": {title: 'Review Rating', "titleColor":"#fff",labelColor:"#fff" }
+          },
+          y: {
+            "aggregate": "sum",
+            "field": "ratings",
+            "type": "quantitative",
+            "axis": {title: "Numbers of Fake Don't Recommand Reviews", "titleColor":"#fff", labelColor:"#fff" }
+          },
+          "tooltip": [{"aggregate": "sum","field": "ratings", "type": "quantitative"}],
+          "color": {"value": "#ffa500"},
+          "fillOpacity": {
+             "condition": [
+              {
+                "selection": "highlight", "value": 1
+              }
+          ],
+          "value": 0.5
+          },
+        }
+      };
+      vegaEmbed('#Ratings_FD', Ratings_FD, options);
+
+
+var Ratings_FR = {
+        $schema: 'https://vega.github.io/schema/vega-lite/v2.0.json',
+        description: 'A simple bar chart with embedded data.',
+        data: {url:"Fake_R.csv"},
+        background:"transparent",
+        mark: 'bar',
+        width:300,
+        height:300,
+        "selection": {
+          "highlight": {"type": "single", "empty": "none", "on": "mouseover"},
+          "select": {"type": "multi"}
+        },
+        "mark": {
+          "type": "bar",
+          "fill": "#ffa500",
+          "cursor": "pointer"
+        },
+        title: {
+                  "text":"Review Rating & Fake Recommand",
+                  "color": "#fff"
+              },
+        encoding: {
+          x: {
+            "bin": "false",
+            "field": "ratings",
+            "type": "ordinal",
+
+            "orient": "right",
+            "axis": {title: 'Review Rating', "titleColor":"#fff",labelColor:"#fff" }
+          },
+          y: {
+            "aggregate": "sum",
+            "field": "ratings",
+            "type": "quantitative",
+            "axis": {title: "Numbers of Fake Recommand Reviews", "titleColor":"#fff", labelColor:"#fff" }
+          },
+          "tooltip": [{"aggregate": "sum","field": "ratings", "type": "quantitative"}],
+          "color": {"value": "#ffa500"},
+          "fillOpacity": {
+             "condition": [
+              {
+                "selection": "highlight", "value": 1
+              }
+          ],
+          "value": 0.5
+          },
+        }
+      };
+      vegaEmbed('#Ratings_FR', Ratings_FR, options);
